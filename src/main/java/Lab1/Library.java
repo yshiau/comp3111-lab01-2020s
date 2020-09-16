27d00d016719b15228f4f3c436d06db7bf20850e
sissi <sissi@DESKTOP-JEAD2IG>
2020-09-16 21:02:06 +0800

    I have made a change

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -5,7 +5,9 @@
 
 public class Library {
 	public static void main(String arg[]) {
-		System.out.println("When there is a file, commit and push.");
+		Library lib = new Library();
+		if(lib.someLibraryMethod())
+			System.out.println("When there is a file, commit and push.");
 	}
     public boolean someLibraryMethod() {
         return true;
